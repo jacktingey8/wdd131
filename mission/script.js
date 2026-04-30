97654f07602fc14e051fdd51d7b3125834cf1d42
jacktingey8 <jacktingey8@gmail.com>
2026-04-30 15:32:04 -0600

    Mission dark mode

---
+++ mission/script.js
@@ -0,0 +1,19 @@
+let mode = document.querySelector('#theme-toggle');
+
+mode.addEventListener('change', theme);
+
+function theme() {
+    // Use the variable 'mode' you defined above
+    if (mode.value === 'dark') {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        document.querySelector('img').src = 'byui-logo-white.png';
+        document.getElementById('info').style.border = '1px solid white';
+    } 
+    else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        document.querySelector('img').src = 'byui-logo-blue.webp';
+        document.getElementById('info').style.border = '1px solid black';
+    }
+}
